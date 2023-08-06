68f17782f01da5554b25e7c7fd4317c0481d2c7e
Marcio Curvello <marciocurvello@gmail.com>
2023-08-06 11:11:25 -0300

    add cronometro

--- src/components/Cron/index.tsx
+++ src/components/Cron/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../Button";
 import Watch from "./Watch";
 import style from "./Cron.module.scss";
 import { ITarefa } from "../../types/tarefa";
+import { tempoParaSegundos } from "../../common/utils/time";
 
 interface Props {
   selecionado: ITarefa | undefined;
 }
 
 export default function Cron({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
-        <Watch />
+        <Watch tempo={tempo} />
       </div>
       <Button text="Iniciar" />
     </div>
